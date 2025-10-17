b21069d8b8497fb760a570343596cd0a8c02749b
Julia Timokhina <juliatimokhina@Julias-MacBook-Pro.local>
2025-10-17 14:43:13 +0300

    feractor(basic 3.4): replace tag selectors with classes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
+var addButton=document.getElementsByClassName("btn")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
@@ -18,6 +18,7 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 var createNewTaskElement=function(taskString){
 
   var listItem=document.createElement("li");
+  listItem.classList.add("tasks-item");
 
   //input (checkbox)
   var checkBox=document.createElement("input");//checkbx
@@ -31,6 +32,7 @@ var createNewTaskElement=function(taskString){
   //button.delete
   var deleteButton=document.createElement("button");//delete button
   var deleteButtonImg=document.createElement("img");//delete button image
+  deleteButton.classList.add("btn", "btn_delete");
 
   label.innerText=taskString;
   label.className="task";
@@ -41,9 +43,9 @@ var createNewTaskElement=function(taskString){
   editInput.className="task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="btn_edit";
+  editButton.classList.add("btn", "btn_edit")
 
-  deleteButton.className="btn_delete";
+//   deleteButton.className="btn_delete";
   deleteButtonImg.src="./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
